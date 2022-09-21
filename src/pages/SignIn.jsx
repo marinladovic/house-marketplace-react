0cb4fef9e4f7dc6c74c09348a89700d86ceeb7e1
marin <mladovic.dev@gmail.com>
2022-09-21 14:59:12 +0200

    signup, login, logout with firebase auth, save user to firestore collection

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
+function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate('/');
+      }
+    } catch (err) {
+      toast.error('Bad User Credentials');
+    }
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <main>
+          <form onSubmit={onSubmitHandler}>
+            {/* Email Input Field */}
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChangeHandler}
+            />
+
+            {/* Password Input Field */}
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="passwordInput"
+                placeholder="Passwrd"
+                id="password"
+                value={password}
+                onChange={onChangeHandler}
+              />
+
+              {/* Toggle Password Visibility Icon */}
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+
+            {/* Link to /forgot-password route */}
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password?
+            </Link>
+
+            {/* Sign In Button */}
+            <div className="signInBar">
+              <p className="signInText">Sign In</p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+
+          {/* Google OAuth Component */}
+
+          {/* Link to /signup route */}
+          <Link to="/signup" className="registerLink">
+            Sign Up Instead
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default SignIn;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,136 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
+function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      /* Create a copy of the form data state */
+      const formDataCopy = { ...formData };
+      /* Remove password not to add it to the database */
+      delete formDataCopy.password;
+      /* Add a timestamp from the server timestamp */
+      formDataCopy.timestamp = serverTimestamp();
+
+      /* Update database and add new user to the 'users' collection */
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
+      navigate('/');
+    } catch (error) {
+      toast.error('Something went wrong with registration');
+    }
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <main>
+          <form onSubmit={onSubmitHandler}>
+            {/* Username Input Field */}
+            <input
+              type="text"
+              className="nameInput"
+              placeholder="Username"
+              id="name"
+              value={name}
+              onChange={onChangeHandler}
+            />
+
+            {/* Email Input Field */}
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChangeHandler}
+            />
+
+            {/* Password Input Field */}
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="passwordInput"
+                placeholder="Passwrd"
+                id="password"
+                value={password}
+                onChange={onChangeHandler}
+              />
+
+              {/* Toggle Password Visibility Icon */}
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+
+            {/* Sign Up Button */}
+            <div className="signUpBar">
+              <p className="signUpText">Sign Up</p>
+              <button className="signUpButton">
+                <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+
+          {/* Google OAuth Component */}
+
+          {/* Sign In Link */}
+          <Link to="/signin" className="registerLink">
+            Sign In Instead
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
